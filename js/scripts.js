20f6f7468055aa75dfe486339ac59c1270383513
Andrew M Westling <a@andrewwestling.com>
2014-11-03 02:30:09 -0800

    clean up scripts.js slightly

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,25 @@ function preload(images) {
 }
 
 function redirectPage() {
-    window.location = linkLocation
-  }
-
-$(document).ready(function() {
-  $(".b").hide();
-  $(".a").hover(function() {
-    if ($(".b").css('display') === "none") {
-      $(".b").delay(300).slideToggle(800, "swing");
-    };
-  });
-});
-
-$(document).ready(function() {
-  $(".b").hide();
-  $("span.n").click(function() {
-    $(".b").slideToggle(800, "swing");
-  });
-});
+  window.location = linkLocation;
+}
 
 $(document).ready(function() {
   $("body").css("display","none");
   $("body").fadeIn(2000);
+  $(".b").hide();
   $("a").click(function(event){
     event.preventDefault();
     linkLocation = this.href;
     $("body").fadeOut(800, redirectPage);
   });
+  $(".a").hover(function() {
+    if ($(".b").css('display') === "none") {
+      $(".b").delay(300).slideToggle(800, "swing");
+    };
+  });
+  $("span.n").click(function() {
+    $(".b").slideToggle(800, "swing");
+  });
 });
-
 
