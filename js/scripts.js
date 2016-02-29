94406fd7faa356d0e99d37c3a3bdb3f24b8f5c9c
Andrew Westling <a@andrewwestling.com>
2016-02-28 23:42:30 -0500

    Bootstrapify ¯\_(ツ)_/¯

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-function preload(images) {
-  $(images).each(function() {
-    $('<img/>')[0].src = this;
-  });
-}
-
 function redirectPage() {
   window.location = linkLocation;
 }
@@ -11,18 +5,13 @@ function redirectPage() {
 $(document).ready(function() {
   $("body").css("display","none");
   $("body").fadeIn(2000);
-  $(".b").hide();
 
   // Fade out when leaving site
   $("a").click(function(event){
     event.preventDefault();
     linkLocation = this.href;
+    $(".site-wrapper").addClass("white");
     $("body").fadeOut(800, redirectPage);
-  });
-
-  // Blurb Toggle
-  $(".show-b").click(function() {
-    $(".b").slideToggle(800, "swing");
   });
 });
 
