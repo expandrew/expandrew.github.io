a80b465d4b532bd08669f1ceff3211d0f0c3e1ac
Andrew Westling <a@andrewwestling.com>
2021-01-12 22:07:23 -0500

    Add base Gatsby setup

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "andrewwestling.com",
+  },
+  plugins: [
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
