b51caef9f37f8c067a7f9e50d51f3faf12a9ee7e
Andrew Westling <a@andrewwestling.com>
2021-02-20 17:07:00 -0500

    Update theme
    - use 6-digit hex codes
    - add fontWeights and lineHeights
    - remove random heading font
    - add breakpoints array

--- next-js/utils/theme.ts
+++ next-js/utils/theme.ts
@@ -1,16 +1,27 @@
 export default {
+  breakpoints: ['40em', '52em', '64em'],
   fonts: {
     body: 'system-ui, sans-serif',
-    heading: '"Avenir Next", sans-serif',
     monospace: 'Menlo, monospace',
   },
+  fontWeights: {
+    body: 400,
+  },
+  lineHeights: {
+    body: 1.5,
+  },
   colors: {
-    text: '#000',
-    background: '#fff',
-    primary: '#33e',
+    text: '#000000',
+    background: '#ffffff',
+    primary: '#f1553a',
     muted: '#f6f6f6',
   },
   styles: {
+    root: {
+      fontFamily: 'body',
+      lineHeight: 'body',
+      fontWeight: 'body',
+    },
     a: {
       color: 'inherit',
       '&:hover': {
