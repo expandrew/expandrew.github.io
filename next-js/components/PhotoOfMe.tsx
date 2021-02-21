a34ef7936e9b0000c0eb4fbcb3d6792825df198f
Andrew Westling <a@andrewwestling.com>
2021-02-20 20:21:52 -0500

    Add "About" content on index page
    - Also remove boilerplate button

---
+++ next-js/components/PhotoOfMe.tsx
@@ -0,0 +1,11 @@
+//@jsx jsx
+import { jsx } from 'theme-ui';
+import NextImage from 'next/Image';
+
+export const PhotoOfMe = () => (
+  <NextImage
+    width={500}
+    height={500}
+    src={'/assets/Andrew-M-Westling-2019-1.jpg'}
+  />
+);
