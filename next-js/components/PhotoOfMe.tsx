325ef60abd342ab2a70af054903ad367cb07e0be
Andrew Westling <a@andrewwestling.com>
2021-02-20 22:30:55 -0500

    Add label to PhotoOfMe

--- next-js/components/PhotoOfMe.tsx
+++ next-js/components/PhotoOfMe.tsx
@@ -1,12 +1,17 @@
 //@jsx jsx
-import { jsx } from 'theme-ui';
+import { jsx, Box, Text } from 'theme-ui';
 import NextImage from 'next/Image';
 
 export const PhotoOfMe = () => (
-  <NextImage
-    width={500}
-    height={500}
-    sx={{ borderRadius: 'large' }}
-    src={'/assets/Andrew-M-Westling-2019-1.jpg'}
-  />
+  <Box>
+    <NextImage
+      width={500}
+      height={500}
+      sx={{ borderRadius: 'large' }}
+      src={'/assets/Andrew-M-Westling-2019-1.jpg'}
+    />
+    <Text variant="label">
+      At Barbican Centre in London, May 2019. Photo by Cecily Devoto
+    </Text>
+  </Box>
 );
