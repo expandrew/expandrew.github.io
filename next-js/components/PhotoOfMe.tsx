14ded23c791c4ad2c0a8886a78bf39e38e2c37b6
Andrew Westling <a@andrewwestling.com>
2021-02-21 12:42:50 -0500

    Improve tag semantics on my photo
    TIL <figure> and <figcaption>: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/figure

--- next-js/components/PhotoOfMe.tsx
+++ next-js/components/PhotoOfMe.tsx
@@ -3,14 +3,15 @@ import { jsx, Box, Text } from 'theme-ui';
 import NextImage from 'next/image';
 
 export const PhotoOfMe = () => (
-  <Box>
+  <Box as="figure">
     <NextImage
       width={500}
       height={500}
       sx={{ borderRadius: 'large' }}
       src={'/assets/Andrew-M-Westling-2019-1.jpg'}
+      alt="Photo of Andrew at Barbican Centre"
     />
-    <Text variant="label">
+    <Text as="figcaption" variant="label">
       At Barbican Centre in London, May 2019. Photo by Cecily Devoto
     </Text>
   </Box>
