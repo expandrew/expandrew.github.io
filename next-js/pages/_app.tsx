e26eedaa96328b977c31a6477b93f182ae7a10fd
Andrew Westling <a@andrewwestling.com>
2021-04-16 20:56:30 -0400

    Add GoatCounter
    When you want analytics but don't want Google Analytics
    
    https://www.goatcounter.com/
    
    Also this helped me: https://stackoverflow.com/questions/55034064/where-to-put-router-events-related-code-in-next-js

--- next-js/pages/_app.tsx
+++ next-js/pages/_app.tsx
@@ -7,13 +7,34 @@ import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import theme from '../theme';
 
+declare const window: any;
+const goatCount = () => {
+  window.goatcounter.count({
+    path: location.pathname + location.search + location.hash,
+  });
+};
+
 export default class App extends NextApp {
+  constructor(props) {
+    super(props);
+    this.props.router?.events?.on('routeChangeComplete', goatCount);
+  }
+
+  componentWillUnmount() {
+    this.props.router.events?.off('routeChangeComplete', goatCount);
+  }
+
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Head>
           <title>andrewwestling.com</title>
+          <script
+            data-goatcounter="https://andrewwestling.goatcounter.com/count"
+            async
+            src="//gc.zgo.at/count.js"
+          ></script>
         </Head>
         <Flex sx={{ minHeight: '100vh', flexDirection: 'column' }}>
           <Header />
